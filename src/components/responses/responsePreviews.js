f0f66d922832b9123b0e82d951b19aa9c064d730
Bhavik <bhaviksingh@gmail.com>
2020-08-12 18:04:48 -0400

    Got the floating responses to work with React code, and adding floating responses one by one after some delay

--- src/components/responses/responsePreviews.js
+++ src/components/responses/responsePreviews.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import ResponseStyles from "../../styles/response.module.css"
 
@@ -15,31 +15,85 @@ export default function ResponsePreviews() {
       }
     }
   `)
-  console.log(data)
 
+  const responseNodeList = data.allResponsesJson.edges.map(({ node }) => node)
+  const listOfIndices = responseNodeList.map(({ node }, index) => index)
+  const waitTime = 5000
+  const [activeNodeIndices, setActiveNodeIndices] = useState([])
+
+  //TODO: We can clean this up by using another state variable probably.
+  //Right now we do this dance where we maintain state for activeIndexes and list of indices (a constant)
+  //Then every single time, to see whats left, we clone listofindices, and remove the stuff from activeIndices..
+  //its fine.. but it can be cleaner :)
+  useEffect(() => {
+    if (activeNodeIndices.length < responseNodeList.length) {
+      let indexArray = listOfIndices
+      activeNodeIndices.forEach(usedIndex => {
+        let usedIndexPos = indexArray.indexOf(usedIndex)
+        indexArray.splice(usedIndexPos, 1)
+      })
+      let randomUnusedIndex =
+        indexArray[Math.floor(Math.random() * indexArray.length)]
+      console.log(
+        `FYI: Adding a response with index of ${randomUnusedIndex} in ${waitTime} ms`
+      )
+      const interval_ID = setInterval(() => {
+        setActiveNodeIndices(activeNodeIndices => [
+          ...activeNodeIndices,
+          randomUnusedIndex,
+        ])
+      }, waitTime)
+      return () => {
+        clearInterval(interval_ID)
+      }
+    }
+  })
+
+  //This then works because it just is returning new things and only the new ones are rendered :)
   function renderAllPreviews() {
-    return data.allResponsesJson.edges.map(({ node }, index) => (
-      <ResponsePreview
-        author={node.author}
-        response={node.response}
-        key={index}
-      ></ResponsePreview>
-    ))
+    return activeNodeIndices.map(nodeIndex => {
+      let node = responseNodeList[nodeIndex]
+      return (
+        <ResponsePreview
+          author={node.author}
+          response={node.response}
+          key={nodeIndex}
+        ></ResponsePreview>
+      )
+    })
   }
 
   return renderAllPreviews() || "None"
 }
 
 function ResponsePreview({ author, response }) {
-  const [bottomPos, setBottomPos] = useState(50)
+  const [bottomPos, setBottomPos] = useState(0)
+  const [animPlaying, setAnimPlaying] = useState(true)
+  const animLength = 100
+
+  useEffect(() => {
+    if (bottomPos < 100 && animPlaying) {
+      const interval_ID = setInterval(() => {
+        setBottomPos(bottomPos => bottomPos + 1)
+      }, animLength)
+      return () => {
+        clearInterval(interval_ID)
+      }
+    }
+  })
 
   return (
     <div
       className={ResponseStyles.responsePreview}
       style={{ bottom: bottomPos + "vh" }}
-      onClick={() => setBottomPos(bottomPos - 1)}
+      onMouseOver={() => {
+        setAnimPlaying(false)
+      }}
+      onMouseOut={() => {
+        setAnimPlaying(true)
+      }}
     >
-      {author} : {response.substring(0, 10) + "..."}
+      {author} : {animPlaying ? response.substring(0, 10) + "..." : response}
     </div>
   )
 }
